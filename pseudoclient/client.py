579b5b2a1e4d626a5060496f27d679dc1b10d98e
Ahmad Waleed <a.waleedothman@gmail.com>
2023-05-06 13:59:22 +0300

    Pseudo-client Randomization

--- pseudoclient/client.py
+++ pseudoclient/client.py
@@ -1,25 +1,34 @@
 from confluent_kafka import Producer
 import socket
-import sys
 import time
+import schema
 import params
+import random
+
+
+def gen_message():
+    fields = schema.schema
+    args = {}
+    for k, v in fields.items():
+        if v['type'] == 'int-range':
+            args[k] = random.randint(v['low'], v['high'])
+        elif v['type'] == 'float-range':
+            args[k] = random.uniform(v['low'], v['high'])
+        elif v['type'] == 'cat':
+            args[k] = random.choice(v['values'])
+        else:
+            raise f'{k} field type not specified or not supported'
+    return str(args)
+
 
 def run_client():
-    server_address = params.server_address
-    msg_size = params.msg_size
     delay = params.client_delay_time
-    schema = params.schema
     conf = {'bootstrap.servers': params.kafka_listeners,
             'client.id': socket.gethostname()}
     producer = Producer(conf)
     topic = params.topic_name
-    try:
-        # Send data
-        while True:
-            message = schema.format(video_id=1050, country='Egypt')
-            producer.produce(topic, value=message.encode("utf-8"))
-            time.sleep(delay)
-
-    finally:
-        print(sys.stderr, 'closing socket')
-        sock.close()
+    # Send data
+    while True:
+        message = gen_message()
+        producer.produce(topic, value=message.encode("utf-8"))
+        time.sleep(delay)
--- pseudoclient/params.py
+++ pseudoclient/params.py
@@ -1,14 +1,4 @@
-number_of_client_threads = 2
+number_of_client_threads = 5
 client_delay_time = 1
-server_address = ('localhost', 10000)
-msg_size = 4096
-fields = ['video_id', 'country']
 kafka_listeners = "kafka1:9092,kafka2:9092,kafka3:9092"
 topic_name = 'deltaTopic'
-
-# create schema string
-schema = '{{'
-for f in fields:
-    schema += f + ':{' + f + '},'
-schema = schema[:-1]
-schema += '}}'
