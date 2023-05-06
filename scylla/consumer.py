55f75ec153b440765228879251e0a544cc065dd0
Mo'men <moamen.hamdy@trianglz.com>
2023-05-06 18:27:42 +0300

    connecting consumer to scylla

--- scylla/consumer.py
+++ scylla/consumer.py
@@ -1,9 +1,6 @@
-# from cassandra.cluster import Cluster
-# from kafka import KafkaConsumer
 from cassandra.cluster import Cluster
-
-import params
 from confluent_kafka import Consumer
+import params
 
 conf = {'bootstrap.servers': params.kafka_listeners,
         'group.id': 'test-consumer',
@@ -11,7 +8,7 @@ conf = {'bootstrap.servers': params.kafka_listeners,
 consumer = Consumer(conf)
 consumer.subscribe(['deltaTopic'])
 
-cluster = Cluster(['scylla1'])
+cluster = Cluster(['scylla'])
 
 session = cluster.connect()
 
@@ -19,7 +16,7 @@ session.execute("CREATE KEYSPACE IF NOT EXISTS scyllakeyspace WITH replication =
                 "'replication_factor': 1}")
 session.execute("USE scyllakeyspace")
 session.execute("CREATE TABLE IF NOT EXISTS mytable (id INT PRIMARY KEY, message TEXT)")
-
+counter = 0
 try:
     while True:
         msg = consumer.poll(1.0)
@@ -31,9 +28,11 @@ try:
             try:
                 # print('Received message: {}'.format(msg.value().decode('utf-8')))
                 # Insert consumed message into ScyllaDB
-                query = "INSERT INTO mytable (id, message) VALUES ({}, '{}')".format(msg.key(), msg.value().decode('utf-8'))
+                query = "INSERT INTO mytable (id, message) VALUES ({}, '{}')".format(counter, msg.value().decode('utf-8'))
+                print(query)
                 session.execute(query)
                 print('Insert successful')
+                counter += 1
             except Exception as e:
                 print('Error while inserting into ScyllaDB: {}'.format(str(e)))
 
