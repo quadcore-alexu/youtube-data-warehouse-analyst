f09b792e76e666a4d223d65e26cb7dfbf306a331
Mariam Ghazi <mariamghazi9@gmail.com>
2023-05-13 14:00:35 +0300

    client produces to different topics

--- pseudoclient/params.py
+++ pseudoclient/params.py
@@ -5,13 +5,23 @@ kafka_listeners = 'kafka1:9092,kafka2:9092,kafka3:9092'
 
 actions = [
     {
-        'topic': 'deltaTopic',
+        'topic': 'views',
         'delay': 1,
         'schema': schemas.view_action
     },
     {
-        'topic': 'deltaTopic',
+        'topic': 'first_view',
         'delay': 1,
-        'schema': schemas.log_action
+        'schema': schemas.log_action.get('type').get('values')[0]
+    },
+    {
+        'topic': 'subscribes',
+        'delay': 1,
+        'schema': schemas.log_action.get('type').get('values')[1]
+    },
+    {
+        'topic': 'likes',
+        'delay': 1,
+        'schema': schemas.log_action.get('type').get('values')[2]
     }
 ]
