13e6501c6b6e436ac5fd619837d2cfb9d7320eb8
Mariam Ghazi <mariamghazi9@gmail.com>
2023-05-14 23:50:18 +0300

    scylla schema created and data insertion

--- scylla/consumer.py
+++ scylla/consumer.py
@@ -1,42 +1,86 @@
 from cassandra.cluster import Cluster
 from confluent_kafka import Consumer
+import threading
 import params
+import json
 
-conf = {'bootstrap.servers': params.kafka_listeners,
-        'group.id': 'test-consumer',
-        'auto.offset.reset': 'earliest'}
-consumer = Consumer(conf)
-consumer.subscribe(['deltaTopic'])
 
-cluster = Cluster(['scylla'])
+def consume_topic(topic):
+    conf = {'bootstrap.servers': params.kafka_listeners,
+            'group.id': 'test-consumer',
+            'auto.offset.reset': 'earliest'}
+    consumer = Consumer(conf)
+    consumer.subscribe([topic])
 
-session = cluster.connect()
+    cluster = Cluster(['scylla'])
+    session = cluster.connect()
+    session.execute("CREATE KEYSPACE IF NOT EXISTS scyllakeyspace WITH replication = {'class': 'SimpleStrategy', "
+                    "'replication_factor': 1}")
+    session.execute("USE scyllakeyspace")
 
-session.execute("CREATE KEYSPACE IF NOT EXISTS scyllakeyspace WITH replication = {'class': 'SimpleStrategy', "
-                "'replication_factor': 1}")
-session.execute("USE scyllakeyspace")
-session.execute("CREATE TABLE IF NOT EXISTS mytable (id INT PRIMARY KEY, message TEXT)")
-counter = 0
-try:
-    while True:
-        msg = consumer.poll(1.0)
-        if msg is None:
-            continue
-        if msg.error():
-            print('Error while consuming message: {}'.format(msg.error()))
-        else:
-            try:
-                # print('Received message: {}'.format(msg.value().decode('utf-8')))
-                # Insert consumed message into ScyllaDB
-                query = "INSERT INTO mytable (id, message) VALUES ({}, '{}')".format(counter, msg.value().decode('utf-8'))
-                print(query)
-                session.execute(query)
-                print('Insert successful')
-                counter += 1
-            except Exception as e:
-                print('Error while inserting into ScyllaDB: {}'.format(str(e)))
+    ddl_file = "schema.cql"
+    with open(ddl_file, "r") as file:
+        ddl_queries = file.read().split(";")
 
-except KeyboardInterrupt:
-    pass
-finally:
-    consumer.close()
+    # Execute the queries
+    for query in ddl_queries:
+        if query.strip():  # Skip empty lines
+            session.execute(query)
+
+    table_name = topic
+    try:
+        while True:
+            msg = consumer.poll(1.0)
+            if msg is None:
+                continue
+            if msg.error():
+                print('Error while consuming message from topic {}: {}'.format(topic, msg.error()))
+            else:
+                try:
+                    message_json = json.loads(msg.value().decode('utf-8'))
+
+                    # Extract the fields from the JSON message
+                    timestamp = message_json.get('timestamp')
+                    user_id = message_json.get('user_id')
+                    user_country = message_json.get('user_country')
+                    user_age = message_json.get('user_age')
+                    video_id = message_json.get('video_id')
+                    channel_id = message_json.get('channel_id')
+                    # video_object = message_json.get('video_object')
+                    seconds_offset = message_json.get('seconds_offset')
+
+                    if seconds_offset is None:
+                        query = "INSERT INTO {} (timestamp, user_id, user_country, user_age, video_id, channel_id) VALUES (%s, %s, %s, %s, %s, %s)".format(
+                            table_name)
+                        session.execute(query, (
+                            timestamp, user_id, user_country, user_age, video_id, channel_id))
+
+                    else:
+                        query = "INSERT INTO {} (timestamp, user_id, user_country, user_age, video_id, channel_id, seconds_offset) VALUES (%s, %s, %s, %s, %s, %s, %s)".format(
+                            table_name)
+                        session.execute(query,
+                                        (timestamp, user_id, user_country, user_age, video_id, channel_id, seconds_offset))
+                    print(query)
+                    print('Insert successful for topic {}'.format(topic))
+
+                except Exception as e:
+                    print('Error while inserting into ScyllaDB for topic {}: {}'.format(topic, str(e)))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        consumer.close()
+
+
+# Define the topics you want to consume from
+topics = params.topics
+
+# Create a separate thread for each topic
+threads = []
+for topic in topics:
+    thread = threading.Thread(target=consume_topic, args=(topic,))
+    thread.start()
+    threads.append(thread)
+
+# Wait for all threads to finish
+for thread in threads:
+    thread.join()
