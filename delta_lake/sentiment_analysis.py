9145f49076ba69fca1e78280db856566bddfef92
Sarah Osama <hylrcs@gmail.com>
2023-07-01 21:34:19 +0300

    bronze comments done

--- delta_lake/sentiment_analysis.py
+++ delta_lake/sentiment_analysis.py
@@ -1,4 +1,5 @@
 from transformers import pipeline
+from pyspark.sql import Column
 
 
 class SentimentAnalysis:
@@ -7,20 +8,23 @@ class SentimentAnalysis:
         self.label_to_score = {"positive": 1, "neutral": 0, "negative": -1}
         self.sentiment_task = pipeline("sentiment-analysis", model=model_path, tokenizer=model_path)
 
-    def classify(self, strings):
+    def classify(self, column):
         """
         Performs sentiment classification on the given input.
         Args:
-            strings (list): A list of texts to classify.
+            column (pyspark.sql.Column): A PySpark DataFrame column containing texts to classify.
         Returns:
-            A list of sentiment scores calculated based on the sentiment classification.
+            A PySpark DataFrame column of sentiment scores calculated based on the sentiment classification.
         Raises:
-            ValueError: If the input is not a list.
+            ValueError: If the input is not a PySpark DataFrame column.
         """
+        if not isinstance(column, Column):
+            raise ValueError("Input must be a PySpark DataFrame column.")
+
+        # Perform sentiment classification on the given column
+        classification = self.sentiment_task(column)
 
         # input = ["قاعد بعيده من امبارح", "ههههه", "ايه القرف دا", "جميل جدا"]
-        if not isinstance(input, list):
-            raise ValueError("Input must be a list.")
-        classification = self.sentiment_task(strings)
-        scores = [c['score'] * self.label_to_score[c['label']] for c in classification]
+
+        scores = classification["score"] * self.label_to_score[classification["label"]]
         return scores
