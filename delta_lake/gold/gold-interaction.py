70c3e0413ef7c96b2498769d34d405fce4961573
Mariam Ghazi <mariamghazi9@gmail.com>
2023-07-01 13:59:27 +0300

    interaction silver and gold tables

---
+++ delta_lake/gold/gold-interaction.py
@@ -0,0 +1,74 @@
+from delta import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import StructType, StructField, StringType, LongType, IntegerType, TimestampType
+from datetime import datetime, timedelta
+import time
+import pyspark
+import os
+
+
+def write_timestamp_checkpoint(spark, first_views_timestamp, path):
+    (spark.createDataFrame([(first_views_timestamp,)], ["last_checked_timestamp"])
+     .write
+     .mode("overwrite")
+     .option("header", "true")
+     .csv(path))
+
+
+def read_timestamp_checkpoint(spark, path):
+    if not os.path.exists(path):
+        return 1683694784
+    checkpoint_timestamp_df = (spark
+                               .read
+                               .option("header", "true")
+                               .csv(path)
+                               .select("last_checked_timestamp"))
+
+    return int(checkpoint_timestamp_df.first()[0])
+
+
+if __name__ == '__main__':
+    builder = pyspark.sql.SparkSession.builder.appName("DeltaApp") \
+                     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+                     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+
+    spark = configure_spark_with_delta_pip(builder).getOrCreate()
+    # Load the Delta table
+    gold_table_path = "hdfs://namenode:9000/tmp/gold_interaction"
+    DeltaTable.createIfNotExists(spark) \
+        .addColumn("max_interaction_time", LongType()) \
+        .addColumn("channel_id", IntegerType()) \
+        .addColumn("country", StringType()) \
+        .location(gold_table_path) \
+        .execute()
+    gold_table = DeltaTable.forPath(spark, gold_table_path)
+
+    while True:
+        # Read the latest records from the silver table that satisfy the condition
+        silver_interaction_table = (spark
+                              .read
+                              .format("delta")
+                              .load("hdfs://namenode:9000/tmp/silver_interaction"))
+
+        aggregated_data = (silver_interaction_table.alias("silver")
+                           .withColumn("interaction", struct("silver.views_count", "silver.hour_offset"))
+        .groupBy("channel_id", "country")
+        .agg(max(col("interaction")).alias("interaction"))
+        .withColumn("views_count", col("interaction.views_count"))
+        .withColumn("hour_offset", col("interaction.hour_offset"))
+                           .select("channel_id", "country", "hour_offset"))
+
+
+        # Merge the aggregated data into the gold table
+        (gold_table.alias("gold")
+         .merge(silver_interaction_table.alias("silver"), "gold.channel_id = silver.channel_id and gold.country = silver.country")
+         .whenMatchedUpdate(set={"hour_offset": "silver.hour_offset"})
+         .whenNotMatchedInsert(values={"channel_id": "silver.cannel_id",
+                                       "country": "silver.country",
+                                       "hour_offset": "silver.hour_offset"})
+         .execute())
+
+        gold_df = spark.read.format("delta").load(gold_table_path)
+        gold_df.show()
+
+        time.sleep(60)
---
+++ delta_lake/silver/silver-interaction.py
@@ -0,0 +1,93 @@
+from delta import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import StructType, StructField, StringType, LongType, IntegerType, TimestampType
+from datetime import datetime, timedelta
+import time
+import pyspark
+import os
+
+
+def write_timestamp_checkpoint(spark, first_views_timestamp, path):
+    (spark.createDataFrame([(first_views_timestamp,)], ["last_checked_timestamp"])
+     .write
+     .mode("overwrite")
+     .option("header", "true")
+     .csv(path))
+
+
+def read_timestamp_checkpoint(spark, path):
+    if not os.path.exists(path):
+        return 1683694784
+    checkpoint_timestamp_df = (spark
+                               .read
+                               .option("header", "true")
+                               .csv(path)
+                               .select("last_checked_timestamp"))
+
+    return int(checkpoint_timestamp_df.first()[0])
+
+
+if __name__ == '__main__':
+    builder = pyspark.sql.SparkSession.builder.appName("DeltaApp") \
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+
+    spark = configure_spark_with_delta_pip(builder).getOrCreate()
+
+    # Load the Delta table
+    silver_table_path = "hdfs://namenode:9000/tmp/silver_interaction"
+    DeltaTable.createIfNotExists(spark) \
+        .addColumn("views_count", LongType()) \
+        .addColumn("channel_id", IntegerType()) \
+        .addColumn("video_id", IntegerType()) \
+        .addColumn("hour_offset", LongType()) \
+        .addColumn("country", StringType()) \
+        .location(silver_table_path) \
+        .execute()
+    silver_table = DeltaTable.forPath(spark, silver_table_path)
+
+    timestamp_checkpoint_path = "hdfs://namenode:9000/tmp/silver_interaction/last_checked_timestamp"
+    first_views_start_timestamp = read_timestamp_checkpoint(spark, timestamp_checkpoint_path)
+
+    while True:
+        # Read the latest records from the bronze table that satisfy the condition
+        bronze_first_views_table = (spark
+                                    .read
+                                    .format("delta")
+                                    .load("hdfs://namenode:9000/tmp/bronze_first_views"))
+
+        first_views_end_timestamp = bronze_first_views_table.agg(
+            max(col("timestamp_seconds"))).collect()[0][0]
+
+        bronze_first_views_table = bronze_first_views_table.where(
+            (col("timestamp_seconds") >= first_views_start_timestamp) & (
+                    col("timestamp_seconds") < first_views_end_timestamp)) \
+            .withColumn("hour_offset", (col('timestamp_seconds') % lit(86400)) // lit(3600))
+
+        # Aggregate the data
+        aggregated_data = (bronze_first_views_table
+                           .groupBy("video_id", "channel_id", "hour_offset", "user_country")
+                           .agg(count("*").alias("views_count"))
+                           .select("video_id", "hour_offset", "views_count", "channel_id", "user_country"))
+
+        aggregated_data.show()
+
+        # Merge the aggregated data into the silver table
+        (silver_table.alias("silver")
+         .merge(aggregated_data.alias("bronze"),
+                "silver.video_id = bronze.video_id and silver.hour_offset = bronze.hour_offset and "
+                "silver.channel_id = bronze.channel_id and silver.country = bronze.user_country")
+         .whenMatchedUpdate(set={"views_count": "silver.views_count + bronze.views_count"})
+         .whenNotMatchedInsert(values={"hour_offset": "bronze.hour_offset", "video_id": "bronze.video_id",
+                                       "channel_id": "bronze.channel_id",
+                                       "country": "bronze.user_country",
+                                       "views_count": "bronze.views_count"})
+         .execute())
+
+        silver_df = spark.read.format("delta").load(silver_table_path)
+        silver_df.show()
+        first_views_start_timestamp = first_views_end_timestamp
+
+        write_timestamp_checkpoint(spark, first_views_end_timestamp, timestamp_checkpoint_path)
+
+        time.sleep(10)
