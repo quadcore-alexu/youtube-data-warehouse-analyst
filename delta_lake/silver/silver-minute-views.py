6650d935f2b30eaf673723e6ad9ea3e9b1345f05
Mo'men <mo2menmohhamdy@gmail.com>
2023-07-05 10:02:04 +0300

    clean silver

--- delta_lake/silver/silver-minute-views.py
+++ delta_lake/silver/silver-minute-views.py
@@ -76,8 +76,6 @@ if __name__ == '__main__':
          .whenNotMatchedInsert(values={"country": "bronze.user_country", "views_count": "bronze.total_views_count"})
          .execute())
 
-        silver_df = spark.read.format("delta").load(silver_table_path)
-        silver_df.show()
         start_timestamp = end_timestamp
 
         bronze_table = (spark
