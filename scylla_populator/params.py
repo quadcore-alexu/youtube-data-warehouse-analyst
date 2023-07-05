1bf0825ad2b529b81c00a9b9d40792fccafcf168
Mo'men <mo2menmohhamdy@gmail.com>
2023-07-05 20:37:30 +0300

    update populator

--- scylla_populator/params.py
+++ scylla_populator/params.py
@@ -4,26 +4,26 @@ number_of_client_threads = 5
 kafka_listeners = 'kafka1:9092,kafka2:9092,kafka3:9092'
 
 actions = [
-    # {
-    #     'topic': 'views',
-    #     'delay': 0.2,
-    #     'schema': schemas.view_action
-    # },
+    {
+        'topic': 'views',
+        'delay': 0.2,
+        'schema': schemas.view_action
+    },
     {
         'topic': 'first_views',
         'delay': 1,
         'schema': schemas.first_views
     }
-    # {
-    #     'topic': 'subscribes',
-    #     'delay': 2,
-    #     'schema': schemas.subscribe
-    # },
-    # {
-    #     'topic': 'likes',
-    #     'delay': 2,
-    #     'schema': schemas.like
-    # },
+    {
+        'topic': 'subscribes',
+        'delay': 2,
+        'schema': schemas.subscribe
+    },
+    {
+        'topic': 'likes',
+        'delay': 2,
+        'schema': schemas.like
+    },
 
     # {
     #     'topic': 'comments',
