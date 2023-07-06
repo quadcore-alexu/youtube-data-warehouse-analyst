9ee2c8502da4a280f127d211cb0da08faed83ffa
Mo'men <mo2menmohhamdy@gmail.com>
2023-07-06 18:42:00 +0300

    fix indentation

--- delta_lake/bronze/bronze-comments.py
+++ delta_lake/bronze/bronze-comments.py
@@ -10,13 +10,11 @@ from sentiment_analysis import SentimentAnalysis
 
 
 def form_log_record(message):
-    return {"timestamp_seconds": int(datetime.timestamp(datetime.now())), "user_id": message["user_id"], "user_country": message["user_country"],
-            "user_age": message["user_age"], "video_id": message["video_id"], "channel_id": message["channel_id"], "comment": message["comment"]}
+    return {"timestamp_seconds": int(datetime.timestamp(datetime.now())), "user_id": message["user_id"], "user_country": message["user_country"], "user_age": message["user_age"], "video_id": message["video_id"], "channel_id": message["channel_id"], "comment": message["comment"]}
 
 
 if __name__ == '__main__':
-    builder = pyspark.sql.SparkSession.builder.appName("DeltaApp").config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension").config(
-        "spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+    builder = pyspark.sql.SparkSession.builder.appName("DeltaApp").config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension").config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
     spark = configure_spark_with_delta_pip(builder).getOrCreate()
 
     data_schema = StructType([
@@ -46,16 +44,13 @@ if __name__ == '__main__':
             continue
         else:
             message_count += 1
-            records.append(form_log_record(
-                json.loads(msg.value().decode('utf-8'))))
+            records.append(form_log_record(json.loads(msg.value().decode('utf-8'))))
             if message_count > batch_size:
                 print("Comments batch written")
                 parsed_df = spark.createDataFrame(records, schema=data_schema).withColumn("id", monotonically_increasing_id())
                 classification = sentiment_analyzer.classify(parsed_df.select("comment").toPandas()["comment"].tolist())
-                classification_df = spark.createDataFrame(classification, schema=IntegerType()).toDF("comment_score").withColumn("id",
-                                                                                                 monotonically_increasing_id())
+                classification_df = spark.createDataFrame(classification, schema=IntegerType()).toDF("comment_score").withColumn("id",monotonically_increasing_id())
                 parsed_df = parsed_df.join(classification_df, on="id", how="inner").drop("id").drop("comment").withColumn("timestamp", from_unixtime(parsed_df["timestamp_seconds"]))
-
                 delta_path = "hdfs://namenode:9000/tmp/bronze_comments"
                 parsed_df.write.format("delta").mode("append").save(delta_path)
                 message_count = 0
